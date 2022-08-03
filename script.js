192f4448b7749c7ff6e12ef218e49ff4e71b00a3
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-03 19:58:04 +0600

    done comment box

---
+++ script.js
@@ -0,0 +1,21 @@
+const submit = document.getElementById('submit-btn').addEventListener('click', function () {
+    const name = document.getElementById('user-name');
+    const userName = name.value;
+    name.value = '';
+
+    const comment = document.getElementById('comment-area');
+    const userComment = comment.value;
+    comment.value = '';
+
+    const commentShow = document.getElementById('comment-output');
+
+    const commentSet = document.createElement('p');
+    commentSet.innerText = userComment;
+    const commentWriter = document.createElement('p');
+    commentWriter.innerText = userName;
+
+    commentShow.appendChild(commentSet)
+
+    commentShow.appendChild(commentWriter)
+
+});
